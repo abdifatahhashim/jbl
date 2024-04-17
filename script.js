8e306394cd555f139e1feb964f60343eb8a90b6f
abdifatahhashim <abdifatahhashim1960@gmail.com>
2024-04-17 14:43:54 +0300

    created a headphones website

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav--menu'),
-      navToggle =document.getElementById('nav--toggle'),
+      navToggle = document.getElementById('nav--toggle'),
       navClose = document.getElementById('nav--close') 
       
     //==menu show===
@@ -20,15 +20,25 @@ if(navClose){
 }
 
 
-
-
-
-
 /*=============== REMOVE MENU MOBILE ===============*/
+const navlink = document.querySelectorAll('.nav__link')
+
+  const linkAction = () => {
+    const navMenu = document.getElementById('nav--menu')
+  //when we click on each nav--link, we remove the show menu click
+  navMenu.classList.remove('show-menu')
+}
+navlink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add(blur-header)
+                         : header.classList.remove(blur-header)
 
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SWIPER FAVORITES ===============*/ 
 
